d077642711e21f88e51375464007a00ae528a4a1
SalimHadhri <91907068+SalimHadhri@users.noreply.github.com>
2021-10-09 17:25:43 +0100

    Add files via upload
    Corrected files

---
+++ ageApprentis.java
@@ -0,0 +1,56 @@
+import java.util.* ;
+
+public class ageApprentis
+{
+	public static void main(String[] args) {
+
+		int[] ageArray = {45, 54, 23, 32, 64, 46, 28, 82} ;
+		
+		int[] arraySorted = trieDecroissant(ageArray ) ;
+		
+		for (int i =0 ; i <ageArray.length ; i++){
+
+		System.out.println(arraySorted [i]);
+		}		
+	}
+
+	public static int maxInt ( int[] array ) {
+
+		int max = array [0];
+		for( int i=1 ; i<array.length ; i++){
+			if (array[i]>max){
+				max=array[i] ;
+			}
+		}		
+	return max; 		
+	}
+
+
+		public static  int[] trieDecroissant(int[] table) {
+
+		int ln = table.length ;
+
+		int[] tabSorted = new int[table.length] ;
+		//int max = maxInt(table) ;
+
+		for (int i = 0 ; i< table.length ;i++) {
+			int[] copy = Arrays.copyOfRange(table, i , table.length);
+			int max = maxInt(copy) ;
+
+			for (int j =1  ; j<table.length ; j++){
+				if (table[j]==max){
+					tabSorted[i]= max;
+					table[j]=table[i] ;
+
+				}
+				}
+
+
+	
+					
+				}
+		return tabSorted ;
+		}
+
+	
+}
